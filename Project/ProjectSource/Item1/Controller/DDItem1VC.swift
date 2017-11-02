d4ed533ea455b2346ceae5ce55043ec8792ada73
wangyuanfei <979433528@qq.com>
2017-11-02 13:40:57 +0800

    add language string file

--- Project/ProjectSource/Item1/Controller/DDItem1VC.swift
+++ Project/ProjectSource/Item1/Controller/DDItem1VC.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
-
+import CryptoSwift
 class DDItem1VC: DDNormalVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        GDStorgeManager.standard.setValue("English", forKey: LLanguageTableName)// .value(forKey: LLanguageTableName)
-        print(GDLanguageManager.titleByKey(key: "title"))
+//        GDStorgeManager.standard.setValue(nil , forKey: LLanguageTableName)// .value(forKey: LLanguageTableName)
+//        print(GDLanguageManager.titleByKey(key: "title"))
+//        let sha1   = "title".sha1()
+//        print(sha1)
+        self.testLocalize()
         // Do any additional setup after loading the view.
     }
 
+    func testLocalize() {
+        let willBeShowLocalIdentifier =  GDLanguageManager.text("language_identifier") // 中文是:"zh_cn"
+        let willBeShowLocal = Locale.init(identifier: "th_th")//localID为"th_th"的本地化对象
+        let showInLocal = Locale.init(identifier: "ch_en")//以中文显示泰文信息
+//        print(Locale.current.regionCode)//以当前区域(国家)显示localID为"zh_cn"的本地化对象的相关信息(国家 ,语言名称等)
+        print(showInLocal.localizedString(forRegionCode: willBeShowLocal.regionCode!))
+        print(showInLocal.localizedString(forLanguageCode: willBeShowLocal.languageCode!))
+        /*
+         输出
+         Optional("泰国")
+         Optional("泰文")
+         */
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
